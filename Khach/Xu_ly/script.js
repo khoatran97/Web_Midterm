8f489dcc2ffa36ab66797e2586a0e3363e69fa6d
khoatran97 <khoatran97@hotmail.com>
2018-04-14 18:03:07 +0700

    [Update] Read Categories from XML
    - Read xml file and list categories in index.html (Khach)

--- Khach/Xu_ly/script.js
+++ Khach/Xu_ly/script.js
@@ -1,18 +1,29 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        Doc_Loai_San_pham(this);
+        Load_Danh_muc(this);
     }
 };
 xhttp.open("GET", "../../Du_lieu/San_pham.xml", true);
 xhttp.send();
 
-function Doc_Loai_San_pham(xml) {
-
-    var Loai = document.getElementById("Loai_San_pham");
-    var danh_muc = xmlRequest.responseXML.getElementByTagName("Danh_muc")
+function Load_Danh_muc(xml) {
+    // Load Cat
+    var Loai = document.getElementById("Danh_muc");
+    var Pho_bien = document.getElementById("Danh_muc_Pho_bien")
+    var data = xhttp.responseXML
+    var danh_muc = data.getElementsByTagName("Danh_muc")
     for (var i = 0; i<danh_muc.length; i++) {
-        var tmp ='<li> <a href="">'+danh_muc[i].getAttribute("Ten")+'</a></li>'
+        var tmp ='<li><a href="#">'+ danh_muc[i].getAttribute("Ten") +'<i class="fas fa-chevron-right"></i></a></li>'
+        var tmp_2 = ` <div class="owl-item">
+                    <div class="popular_category d-flex flex-column align-items-center justify-content-center">
+                        <div class="popular_category_image"><img src="../../Hinh_anh/Danh_muc/`+danh_muc[i].getAttribute("Ma")+`.png" alt=""></div>
+                        <div class="popular_category_text">`+danh_muc[i].getAttribute("Ten")+`</div>
+                    </div>
+                </div> `
         Loai.innerHTML += tmp;
+        Pho_bien.innerHTML += tmp_2;
     }
+
+    // Load Popular Cat
 }
