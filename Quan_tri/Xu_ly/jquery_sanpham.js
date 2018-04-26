a5da6c0f5f2cd3c14250f857503077abf14cfb59
Nguyễn Ngọc Xuân Mỹ <nnxmy97@gmail.com>
2018-04-26 09:05:06 +0700

    [Modify] Phân hệ quản trị
    -chỉnh các file trong folder xử lý

--- Quan_tri/Xu_ly/jquery_sanpham.js
+++ Quan_tri/Xu_ly/jquery_sanpham.js
@@ -1,114 +1,12 @@
 $(document).ready(function() {
-
-	setupTuideocheo();
-	setupTuideolung();
-	//setupTuixach();
-	//setupBopvi();
-	//setupBalo();
-	setupSanpham();
+	$('.dropdown-menu button').click(function(){
+		var value=$(this).attr('id');
+		if(value==="nuttdc"){
+			fillTable(tdcProd);
+		}
+		else if(value==="nuttdl"){
+			fillTable(tdlProd);
+		}
+		else fillTable(allProd);
 });
-
-
-function setupSanpham()
-{
-	$('#tuideocheo').hide();
-	$('#tuideolung').hide();
-	//$('#tuixach').hide();
-	//$('#bopvi').hide();
-	//$('#balo').hide();
-	$('#nuttc').click(function(){
-	    $('#tuideocheo').hide();
-	    $('#tuideolung').hide();
-	    //$('#tuixach').hide();
-	    //$('#bopvi').hide();
-	    //$('#balo').hide();
-		$('#tatca').show();
-	});
-}
-
-function setupTuideocheo()
-{
-	$('#tuideocheo').hide();
-	$('#tuideolung').hide();
-	//$('#tuixach').hide();
-	//$('#bopvi').hide();
-	//$('#balo').hide();
-	$('#nuttdc').click(function(){
-		
-	    $('#tuideolung').hide();
-	    //$('#tuixach').hide();
-	    //$('#bopvi').hide();
-	    //$('#balo').hide();
-		$('#tatca').hide();
-		$('#tuideocheo').show();
-	});
-}
-
-function setupTuideolung()
-{
-	$('#tuideocheo').hide();
-	$('#tuideolung').hide();
-	//$('#tuixach').hide();
-	//$('#bopvi').hide();
-	//$('#balo').hide();
-	$('#nuttdl').click(function(){
-		$('#tuideocheo').hide();
-	    $('#tuideolung').show();
-	   // $('#tuixach').hide();
-	    //$('#bopvi').hide();
-	   // $('#balo').hide();
-		$('#tatca').hide();
-	});
-}
-
-/*function setupTuixach()
-{
-	$('#tuideotreo').hide();
-	$('#tuideolung').hide();
-	$('#tuixach').hide();
-	$('#bopvi').hide();
-	$('#balo').hide();
-	$('#nuttx').click(function(){
-		$('#tuideotreo').hide();
-	    $('#tuideolung').hide();
-	    $('#tuixach').show();
-	    $('#bopvi').hide();
-	    $('#balo').hide();
-		$('#tatca').hide();
-	});
-}
-
-
-function setupBopvi()
-{
-	$('#tuideotreo').hide();
-	$('#tuideolung').hide();
-	$('#tuixach').hide();
-	$('#bopvi').hide();
-	$('#balo').hide();
-	$('#nutbv').click(function(){
-		$('#tuideotreo').hide();
-	    $('#tuideolung').hide();
-	    $('#tuixach').hide();
-	    $('#bopvi').show();
-	    $('#balo').hide();
-		$('#tatca').hide();
-	});
-}
-
-function setupBalo()
-{
-	$('#tuideotreo').hide();
-	$('#tuideolung').hide();
-	$('#tuixach').hide();
-	$('#bopvi').hide();
-	$('#balo').hide();
-	$('#nutbl').click(function(){
-		$('#tuideotreo').hide();
-	    $('#tuideolung').hide();
-	    $('#tuixach').hide();
-	    $('#bopvi').hide();
-	    $('#balo').show();
-		$('#tatca').hide();
-	});
-}*/
+});
--- Quan_tri/Xu_ly/script_sanpham.js
+++ Quan_tri/Xu_ly/script_sanpham.js
@@ -1,5 +1,4 @@
-function createTable() {
-    var orders = [
+var allProd = [
         {code: '123', name: 'Tý', type: 'Túi đeo chéo', price: '100.000', count:'40', producer:'VERCASE'},
         {code: '234', name: 'Sửu', type: 'Túi đeo lưng', price: '200.000', count:'100', producer: 'GIVENCHY'},
         {code: '345', name: 'Dần', type: 'Túi xách', price: '300.000', count:'60', producer: 'GUCCI'},
@@ -13,6 +12,25 @@ function createTable() {
         {code: '567', name: 'Thìn', type: 'Balo', price: '300.000', count:'90', producer: 'TOPBAG'},
         {code: '678', name: 'Tỵ', type: 'Túi đeo lưng', price: '300.000', count:'140', producer: 'BELLABAG'}
     ];
+    var tdcProd = [
+        {code: '123', name: 'Tý', type: 'Túi đeo chéo', price: '100.000', count:'40', producer:'VERCASE'},
+        {code: '234', name: 'Sửu', type: 'Túi đeo chéo', price: '200.000', count:'100', producer: 'GIVENCHY'},
+        {code: '345', name: 'Dần', type: 'Túi đeo chéo', price: '300.000', count:'60', producer: 'GUCCI'},
+        {code: '456', name: 'Mẹo',type: 'Túi đeo chéo', price: '300.000', count:'20', producer: 'PRADA'},
+        {code: '567', name: 'Thìn', type: 'Túi đeo chéo', price: '300.000', count:'90', producer: 'TOPBAG'},
+        {code: '678', name: 'Tỵ', type: 'Túi đeo chéo', price: '300.000', count:'140', producer: 'BELLABAG'}
+    ];
+    var tdlProd = [
+        {code: '123', name: 'Tý', type: 'Túi đeo lưng', price: '100.000', count:'40', producer:'VERCASE'},
+        {code: '234', name: 'Sửu', type: 'Túi đeo lưng', price: '200.000', count:'100', producer: 'GIVENCHY'},
+        {code: '345', name: 'Dần', type: 'Túi đeo lưng', price: '300.000', count:'60', producer: 'GUCCI'},
+        {code: '456', name: 'Mẹo',type: 'Túi đeo lưng', price: '300.000', count:'20', producer: 'PRADA'},
+        {code: '567', name: 'Thìn', type: 'Túi đeo lưng', price: '300.000', count:'90', producer: 'TOPBAG'},
+        {code: '678', name: 'Tỵ', type: 'Túi đeo lưng', price: '300.000', count:'140', producer: 'BELLABAG'},
+        {code: '123', name: 'Tý', type: 'Túi đeo lưng', price: '100.000', count:'40', producer:'VERCASE'},
+        {code: '234', name: 'Sửu', type: 'Túi đeo lưng', price: '200.000', count:'100', producer: 'GIVENCHY'}
+    ];
+function fillTable(orders) {
     //đưa chuỗi vào bảng
      var html = "<table id='tatca'>";
      html+="<tr id='tieude'>";
@@ -38,76 +56,3 @@ function createTable() {
      html+="</table>";
      document.getElementById("t00").innerHTML = html;
 }
-
-function Tabletdc() {
-    var orders = [
-        {code: '123', name: 'Tý', type: 'Túi đeo chéo', price: '100.000', count:'40', producer:'VERCASE'},
-        {code: '234', name: 'Sửu', type: 'Túi đeo chéo', price: '200.000', count:'100', producer: 'GIVENCHY'},
-        {code: '345', name: 'Dần', type: 'Túi đeo chéo', price: '300.000', count:'60', producer: 'GUCCI'},
-        {code: '456', name: 'Mẹo',type: 'Túi đeo chéo', price: '300.000', count:'20', producer: 'PRADA'},
-        {code: '567', name: 'Thìn', type: 'Túi đeo chéo', price: '300.000', count:'90', producer: 'TOPBAG'},
-        {code: '678', name: 'Tỵ', type: 'Túi đeo chéo', price: '300.000', count:'140', producer: 'BELLABAG'}
-    ];
-    //đưa chuỗi vào bảng
-     var html = "<table id='tuideocheo'>";
-     html+="<tr>";
-         html+="<th id='c1'>Mã sản phẩm</th>";
-         html+="<th>Tên sản phẩm</th>";
-         html+="<th>Loại sản phẩm</th>";
-         html+="<th>Giá sản phẩm</th>";
-         html+="<th>Số lượng sản phẩm</th>";
-         html+="<th>Nhà phân phối</th>";
-   
-         html+="</tr>";
-
-     for (var i = 0; i < orders.length; i++) {
-        html+="<tr>";
-        html+="<td>"+orders[i].code+"</td>";
-        html+="<td>"+orders[i].name+"</td>";
-        html+="<td>"+orders[i].type+"</td>";
-        html+="<td>"+orders[i].price+"</td>";
-        html+="<td>"+orders[i].count+"</td>";
-        html+="<td>"+orders[i].producer+"</td>";
-        html+="</tr>";
-     }
-     html+="</table>";
-     document.getElementById("t01").innerHTML = html;
-}
-
-function Tabletdl() {
-    var orders = [
-        {code: '123', name: 'Tý', type: 'Túi đeo lưng', price: '100.000', count:'40', producer:'VERCASE'},
-        {code: '234', name: 'Sửu', type: 'Túi đeo lưng', price: '200.000', count:'100', producer: 'GIVENCHY'},
-        {code: '345', name: 'Dần', type: 'Túi đeo lưng', price: '300.000', count:'60', producer: 'GUCCI'},
-        {code: '456', name: 'Mẹo',type: 'Túi đeo lưng', price: '300.000', count:'20', producer: 'PRADA'},
-        {code: '567', name: 'Thìn', type: 'Túi đeo lưng', price: '300.000', count:'90', producer: 'TOPBAG'},
-        {code: '678', name: 'Tỵ', type: 'Túi đeo lưng', price: '300.000', count:'140', producer: 'BELLABAG'},
-        {code: '123', name: 'Tý', type: 'Túi đeo lưng', price: '100.000', count:'40', producer:'VERCASE'},
-        {code: '234', name: 'Sửu', type: 'Túi đeo lưng', price: '200.000', count:'100', producer: 'GIVENCHY'}
-    ];
-    //đưa chuỗi vào bảng
-     var html = "<table id='tuideolung'>";
-     html+="<tr>";
-         html+="<th id='c1'>Mã sản phẩm</th>";
-         html+="<th>Tên sản phẩm</th>";
-         html+="<th>Loại sản phẩm</th>";
-         html+="<th>Giá sản phẩm</th>";
-         html+="<th>Số lượng sản phẩm</th>";
-         html+="<th>Nhà phân phối</th>";
-   
-         html+="</tr>";
-
-     for (var i = 0; i < orders.length; i++) {
-        html+="<tr>";
-        html+="<td>"+orders[i].code+"</td>";
-        html+="<td>"+orders[i].name+"</td>";
-        html+="<td>"+orders[i].type+"</td>";
-        html+="<td>"+orders[i].price+"</td>";
-        html+="<td>"+orders[i].count+"</td>";
-        html+="<td>"+orders[i].producer+"</td>";
-        html+="</tr>";
-     }
-     html+="</table>";
-     document.getElementById("t02").innerHTML = html;
-}
-
