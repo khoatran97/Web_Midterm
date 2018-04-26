d0e20a3d4fa52d929d38bb4046b6f034f6953422
nthuhuong <thuhuongnguyen160197@gmail.com>
2018-04-26 11:28:31 +0700

    [Update] Quản lý sản phẩm

--- Quan_tri/Xu_ly/jquery_sanpham.js
+++ Quan_tri/Xu_ly/jquery_sanpham.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.dropdown-menu button').click(function(){
+	$('#lsp.dropdown-menu button').click(function(){
 		var value=$(this).attr('id');
 		if(value==="nuttdc"){
 			fillTable(tdcProd);
@@ -7,6 +7,39 @@ $(document).ready(function() {
 		else if(value==="nuttdl"){
 			fillTable(tdlProd);
 		}
+		else if(value==="nuttx"){
+			fillTable(txProd);
+		}
+		else if(value==="nutbv")
+		{
+			fillTable(bvProd);
+		}
+		else if(value==="nutbl"){
+			fillTable(blProd);
+		}
 		else fillTable(allProd);
-});
+	});
+	$('#nsx.dropdown-menu button').click(function(){
+		var value=$(this).attr('id');
+		if(value==="nutgc"){
+			fillTable(gcProd);
+		}
+		else if(value==="nuttb"){
+			fillTable(tbProd);
+		}
+		else if(value==="nutbl"){
+			fillTable(blbProd);
+		}
+		else if(value==="nutpd"){
+			fillTable(prdProd);
+		}
+		else if(value==="nutgc"){
+			fillTable(gcProd);
+		}
+		else if(value==="nutvc"){
+			fillTable(vcProd);
+		}
+		else fillTable(allProd);
+	});
+		
 });
--- Quan_tri/Xu_ly/script_sanpham.js
+++ Quan_tri/Xu_ly/script_sanpham.js
@@ -30,6 +30,102 @@ var allProd = [
         {code: '123', name: 'Tý', type: 'Túi đeo lưng', price: '100.000', count:'40', producer:'VERCASE'},
         {code: '234', name: 'Sửu', type: 'Túi đeo lưng', price: '200.000', count:'100', producer: 'GIVENCHY'}
     ];
+
+    var txProd = [
+        {code: '123', name: 'Tý', type: 'Túi xách', price: '100.000', count:'40', producer:'VERCASE'},
+        {code: '234', name: 'Sửu', type: 'Túi xách', price: '200.000', count:'100', producer: 'GIVENCHY'},
+        {code: '345', name: 'Dần', type: 'Túi xách', price: '300.000', count:'60', producer: 'GUCCI'},
+        {code: '456', name: 'Mẹo',type: 'Túi xách', price: '300.000', count:'20', producer: 'PRADA'},
+        {code: '567', name: 'Thìn', type: 'Túi xách', price: '300.000', count:'90', producer: 'TOPBAG'},
+        {code: '678', name: 'Tỵ', type: 'Túi xách', price: '300.000', count:'140', producer: 'BELLABAG'}
+    ];
+
+    var bvProd = [
+        {code: '123', name: 'Tý', type: 'Bóp, ví', price: '100.000', count:'40', producer:'VERCASE'},
+        {code: '234', name: 'Sửu', type: 'Bóp, ví', price: '200.000', count:'100', producer: 'GIVENCHY'},
+        {code: '345', name: 'Dần', type: 'Bóp, ví', price: '300.000', count:'60', producer: 'GUCCI'},
+        {code: '456', name: 'Mẹo',type: 'Bóp, ví', price: '300.000', count:'20', producer: 'PRADA'}
+    ];
+
+    var blProd = [
+        {code: '123', name: 'Tý', type: 'Balo', price: '100.000', count:'40', producer:'VERCASE'},
+        {code: '234', name: 'Sửu', type: 'Balo', price: '200.000', count:'100', producer: 'GIVENCHY'},
+        {code: '345', name: 'Dần', type: 'Balo', price: '300.000', count:'60', producer: 'GUCCI'},
+        {code: '456', name: 'Mẹo',type: 'Balo', price: '300.000', count:'20', producer: 'PRADA'},
+        {code: '567', name: 'Thìn', type: 'Balo', price: '300.000', count:'90', producer: 'TOPBAG'},
+        {code: '678', name: 'Tỵ', type: 'Balo', price: '300.000', count:'140', producer: 'BELLABAG'},
+        {code: '123', name: 'Tý', type: 'Balo', price: '100.000', count:'40', producer:'VERCASE'},
+        {code: '234', name: 'Sửu', type: 'Balo', price: '200.000', count:'100', producer: 'GIVENCHY'},
+        {code: '345', name: 'Dần', type: 'Balo', price: '300.000', count:'60', producer: 'GUCCI'},
+        {code: '456', name: 'Mẹo',type: 'Balo', price: '300.000', count:'20', producer: 'PRADA'},
+        {code: '567', name: 'Thìn', type: 'Balo', price: '300.000', count:'90', producer: 'TOPBAG'},
+        {code: '678', name: 'Tỵ', type: 'Balo', price: '300.000', count:'140', producer: 'BELLABAG'}
+    ];
+
+    var gcProd = [
+        {code: '123', name: 'Tý', type: 'Balo', price: '100.000', count:'40', producer:'GUCCI'},
+        {code: '234', name: 'Sửu', type: 'Balo', price: '200.000', count:'100', producer: 'GUCCI'},
+        {code: '345', name: 'Dần', type: 'Balo', price: '300.000', count:'60', producer: 'GUCCI'},
+        {code: '456', name: 'Mẹo',type: 'Balo', price: '300.000', count:'20', producer: 'GUCCI'},
+        {code: '567', name: 'Thìn', type: 'Balo', price: '300.000', count:'90', producer: 'GUCCI'},
+        {code: '678', name: 'Tỵ', type: 'Balo', price: '300.000', count:'140', producer: 'GUCCI'},
+        {code: '123', name: 'Tý', type: 'Balo', price: '100.000', count:'40', producer:'GUCCI'},
+        {code: '234', name: 'Sửu', type: 'Balo', price: '200.000', count:'100', producer: 'GUCCI'},
+        {code: '345', name: 'Dần', type: 'Balo', price: '300.000', count:'60', producer: 'GUCCI'},
+        {code: '456', name: 'Mẹo',type: 'Balo', price: '300.000', count:'20', producer: 'GUCCI'},
+        {code: '567', name: 'Thìn', type: 'Balo', price: '300.000', count:'90', producer: 'GUCCI'},
+        {code: '678', name: 'Tỵ', type: 'Balo', price: '300.000', count:'140', producer: 'GUCCI'}
+    ];
+
+
+    var tbProd = [
+        {code: '123', name: 'Tý', type: 'Bóp, ví', price: '100.000', count:'40', producer:'TOPBAG'},
+        {code: '234', name: 'Sửu', type: 'Bóp, ví', price: '200.000', count:'100', producer: 'TOPBAG'},
+        {code: '345', name: 'Dần', type: 'Bóp, ví', price: '300.000', count:'60', producer: 'TOPBAG'},
+        {code: '456', name: 'Mẹo',type: 'Bóp, ví', price: '300.000', count:'20', producer: 'TOPBAG'}
+    ];
+
+    var blbProd = [
+        {code: '123', name: 'Tý', type: 'Túi đeo lưng', price: '100.000', count:'40', producer:'BELLABAG'},
+        {code: '234', name: 'Sửu', type: 'Túi đeo lưng', price: '200.000', count:'100', producer: 'BELLABAG'},
+        {code: '345', name: 'Dần', type: 'Túi đeo lưng', price: '300.000', count:'60', producer: 'BELLABAG'},
+        {code: '456', name: 'Mẹo',type: 'Túi đeo lưng', price: '300.000', count:'20', producer: 'BELLABAG'},
+        {code: '567', name: 'Thìn', type: 'Túi đeo lưng', price: '300.000', count:'90', producer: 'BELLABAG'},
+        {code: '678', name: 'Tỵ', type: 'Túi đeo lưng', price: '300.000', count:'140', producer: 'BELLABAG'},
+        {code: '123', name: 'Tý', type: 'Túi đeo lưng', price: '100.000', count:'40', producer:'BELLABAG'},
+        {code: '234', name: 'Sửu', type: 'Túi đeo lưng', price: '200.000', count:'100', producer: 'BELLABAG'}
+    ];
+
+    var prdProd = [
+        {code: '123', name: 'Tý', type: 'Túi xách', price: '100.000', count:'40', producer:'PRADA'},
+        {code: '234', name: 'Sửu', type: 'Túi xách', price: '200.000', count:'100', producer: 'PRADA'},
+        {code: '345', name: 'Dần', type: 'Túi xách', price: '300.000', count:'60', producer: 'PRADA'},
+        {code: '567', name: 'Thìn', type: 'Túi xách', price: '300.000', count:'90', producer: 'PRADA'},
+        {code: '678', name: 'Tỵ', type: 'Túi xách', price: '300.000', count:'140', producer: 'PRADA'}
+    ];
+
+    var gcProd = [
+        {code: '123', name: 'Tý', type: 'Túi đeo chéo', price: '100.000', count:'40', producer:'GIVENCHY'},
+        {code: '234', name: 'Sửu', type: 'Túi đeo chéo', price: '200.000', count:'100', producer: 'GIVENCHY'},
+        {code: '345', name: 'Dần', type: 'Túi đeo chéo', price: '300.000', count:'60', producer: 'GIVENCHY'},
+        {code: '456', name: 'Mẹo',type: 'Túi đeo chéo', price: '300.000', count:'20', producer: 'GIVENCHY'}
+    ];
+
+     var vcProd = [
+        {code: '123', name: 'Tý', type: 'Balo', price: '100.000', count:'40', producer:'VERCASE'},
+        {code: '234', name: 'Sửu', type: 'Balo', price: '200.000', count:'100', producer: 'VERCASE'},
+        {code: '345', name: 'Dần', type: 'Balo', price: '300.000', count:'60', producer: 'VERCASE'},
+        {code: '456', name: 'Mẹo',type: 'Balo', price: '300.000', count:'20', producer: 'VERCASE'},
+        {code: '567', name: 'Thìn', type: 'Balo', price: '300.000', count:'90', producer: 'VERCASE'},
+        {code: '678', name: 'Tỵ', type: 'Balo', price: '300.000', count:'140', producer: 'VERCASE'},
+        {code: '123', name: 'Tý', type: 'Balo', price: '100.000', count:'40', producer:'VERCASE'},
+        {code: '234', name: 'Sửu', type: 'Balo', price: '200.000', count:'100', producer: 'VERCASE'},
+        {code: '345', name: 'Dần', type: 'Balo', price: '300.000', count:'60', producer: 'VERCASE'},
+        {code: '456', name: 'Mẹo',type: 'Balo', price: '300.000', count:'20', producer: 'VERCASE'},
+        {code: '567', name: 'Thìn', type: 'Balo', price: '300.000', count:'90', producer: 'VERCASE'},
+        {code: '678', name: 'Tỵ', type: 'Balo', price: '300.000', count:'140', producer: 'VERCASE'}
+    ];
+
 function fillTable(orders) {
     //đưa chuỗi vào bảng
      var html = "<table id='tatca'>";
@@ -56,3 +152,5 @@ function fillTable(orders) {
      html+="</table>";
      document.getElementById("t00").innerHTML = html;
 }
+
+
