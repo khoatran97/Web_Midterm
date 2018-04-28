26bd2b3b7eae5f4446b81824e995abdf3f326174
khoatran97 <khoatran97@hotmail.com>
2018-04-28 12:47:01 +0700

    [Update] Detail page
    - Add an area to display the products in the same category with the main product

--- Khach/Xu_ly/script_Chi_tiet.js
+++ Khach/Xu_ly/script_Chi_tiet.js
@@ -19,6 +19,8 @@ function Load_Chi_tiet(xml) {
     var data = xhttp.responseXML;
     var san_pham = data.getElementsByTagName("San_pham");
     var ds_thong_ke = data.getElementsByTagName("Thong_ke");
+
+    var cung_loai = document.getElementById("Cung_loai");
     var thong_ke = null;
     for (var i=0; i<ds_thong_ke.length; i++)
         {
@@ -41,6 +43,26 @@ function Load_Chi_tiet(xml) {
             <p><b>Số lượt mua: `+thong_ke.getAttribute("Luot_mua")+`</b></p>`
 
             document.getElementsByName("Hinh").forEach((img) => img.setAttribute("src", "../../Hinh_anh/San_pham/"+san_pham[i].getAttribute("Ma")+".jpg"))
+            
+            var ds_cung_loai = san_pham[i].parentElement.childNodes;
+            var count = 0;
+
+            for (var k = 0; k<ds_cung_loai.length && count<5; k++) {
+                if (ds_cung_loai[k].nodeType == 3 || ds_cung_loai[k].getAttribute("Ma") == Ma_San_pham) {
+                    continue;
+                }
+                cung_loai.innerHTML += 
+                    `<div class="owl-item">
+                        <div class="viewed_item discount d-flex flex-column align-items-center justify-content-center text-center">
+                            <div class="viewed_image"><img src="../../Hinh_anh/San_pham/`+ds_cung_loai[k].getAttribute("Ma")+`.jpg" alt=""></div>
+                            <div class="viewed_content text-center">
+                                <div class="viewed_price">`+ds_cung_loai[k].getAttribute("Gia")+`</span></div>
+                                <div class="viewed_name"><a href="#">`+ds_cung_loai[k].getAttribute("Ten")+`</a></div>
+                            </div>
+                        </div>
+                    </div>`;
+                count++;
+            }
             break;
         }
     }
--- Nguoi_dung/Xu_ly/script_Chi_tiet.js
+++ Nguoi_dung/Xu_ly/script_Chi_tiet.js
@@ -19,6 +19,8 @@ function Load_Chi_tiet(xml) {
     var data = xhttp.responseXML;
     var san_pham = data.getElementsByTagName("San_pham");
     var ds_thong_ke = data.getElementsByTagName("Thong_ke");
+
+    var cung_loai = document.getElementById("Cung_loai");
     var thong_ke = null;
     for (var i=0; i<ds_thong_ke.length; i++)
         {
@@ -44,6 +46,26 @@ function Load_Chi_tiet(xml) {
             var tmp = `<button type="button" class="button cart_button" onclick="sessionStorage.setItem(`+i+`, `+i+`); alert("Thêm vào giỏ hàng thành công!");>Thêm vào giỏ</button>
                                     <div class="product_fav"><i class="fas fa-heart"></i></div>`;
             document.getElementById("bton").innerHTML = tmp;
+
+            var ds_cung_loai = san_pham[i].parentElement.childNodes;
+            var count = 0;
+
+            for (var k = 0; k<ds_cung_loai.length && count<5; k++) {
+                if (ds_cung_loai[k].nodeType == 3 || ds_cung_loai[k].getAttribute("Ma") == Ma_San_pham) {
+                    continue;
+                }
+                cung_loai.innerHTML += 
+                    `<div class="owl-item">
+                        <div class="viewed_item discount d-flex flex-column align-items-center justify-content-center text-center">
+                            <div class="viewed_image"><img src="../../Hinh_anh/San_pham/`+ds_cung_loai[k].getAttribute("Ma")+`.jpg" alt=""></div>
+                            <div class="viewed_content text-center">
+                                <div class="viewed_price">`+ds_cung_loai[k].getAttribute("Gia")+`</span></div>
+                                <div class="viewed_name"><a href="#">`+ds_cung_loai[k].getAttribute("Ten")+`</a></div>
+                            </div>
+                        </div>
+                    </div>`;
+                count++;
+            }
             break;
         }
     }
