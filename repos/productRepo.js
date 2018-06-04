9fb403d86186ab2b0596e47303b24e4b8184e6d4
khoatran97 <khoatran97@hotmail.com>
2018-06-04 16:23:43 +0700

    [Add] Chi_tiet page
    - Add Chi_tiet page to show the detail of product
    - Edit app.js to display the prize in Vietnamese currency format

--- repos/productRepo.js
+++ repos/productRepo.js
@@ -1,8 +1,36 @@
 var db = require('../fn/dbAccess');
 
 module.exports.loadAll = () => {
-	var sql=`select * from sanpham`;
-	return db.load(sql);
+    var sql = `select * from sanpham`;
+    return db.load(sql);
+}
+
+module.exports.loadById = (id) => {
+    var sql = `select * from sanpham where masanpham = ` + id;
+    return db.load(sql);
+}
+
+module.exports.loadByBrand = (brandId, row = null) => {
+    if (row != undefined && row != null) {
+        var sql = `select * from sanpham where mathuonghieu = ` + brandId + ` limit ` + row;
+        return db.load(sql);
+    }
+    var sql = `select * from sanpham where mathuonghieu = ` + brandId;
+    return db.load(sql);
+}
+
+module.exports.loadByCategory = (categoryId, row = null) => {
+    if (row != undefined && row != null) {
+        var sql = `select * from sanpham where maloai = ` + categoryId + ` limit ` + row;
+        return db.load(sql);
+    }
+    var sql = `select * from sanpham where maloai = ` + categoryId;
+    return db.load(sql);
+}
+
+module.exports.loadByCategoryBrand = (categoryId, brandId) => {
+    var sql = `select * from sanpham where (maloai = ` + categoryId + ` and mathuonghieu = ` + brandId + `)`;
+    return db.load(sql);
 }
 
 module.exports.loadByView = () => {
